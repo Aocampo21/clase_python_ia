bcdf27b8e53516ef2cd5b0c4c42110caeeb9faea
Amanda Ocampo <aocampo@cuc.edu.co>
2021-09-03 04:42:31 -0500

    tipos de datos, conversions y concatenaciones

--- clase_uno.py
+++ clase_uno.py
@@ -4,4 +4,75 @@ Created on Thu Sep  2 04:19:55 2021
 
 @author: amand
 """
-print ('Hello world')
+
+print('Hello world')
+
+# Variables
+a = 5
+print(type(a))
+a = "Hola"
+print(type(a))
+a = 3.5
+print(type(a))
+a = True
+print(type(a))
+
+# Suma
+a = 5
+b = 2
+c = a + b
+print(c)
+
+# Resta
+a = 5
+b = 2
+c = a - b
+print(c)
+
+# Multiplicacion
+a = 5
+b = 2
+c = a * b
+print(c)
+
+# Division
+a = 5
+b = 2
+c = a / b
+print(c)
+
+a = 5
+b = 2
+c = a // b
+print(c)
+
+# Potencia
+a = 5
+b = 2
+c = a ** b
+print(c)
+
+# Raiz
+a = 5
+c = a ** (1/2)
+print(c)
+
+# import math
+# raiz = math.sqrt(25)
+# print(raiz)
+
+# Conversiones
+
+a = 3
+y = str(a)
+print(y)
+print(type(y))
+
+# Concatenaciones
+
+a = 'hola'
+b = 'mundo'
+c = a + '' + b
+
+
+
