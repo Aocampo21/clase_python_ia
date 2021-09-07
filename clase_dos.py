3e7571aade06ab05116d93e467f2b0367640889d
Amanda Ocampo <aocampo@cuc.edu.co>
2021-09-06 20:04:38 -0500

    Ejercicios condicionales

--- clase_dos.py
+++ clase_dos.py
@@ -22,3 +22,53 @@ print(True or True)
 print(True or False)
 print(False or True)
 print(False or False)
+
+# Negacion
+
+print(not True)
+print(not False)
+
+# Mas de dos condiciones al mismo tiempo
+
+print(True and False or False or True or True)
+print(True and True) or False or (True or True)
+
+# Estructura if
+x = -1
+if(x > 0):
+    print('1')
+else:
+    print('2')
+print('3')
+
+# HUA que dada la edad de una persona indique si es mayor
+# o menor de edad
+
+edad = int(input('Digite su edad: '))
+if(edad >= 18):
+    print('Usted es mayor de edad')
+else:
+    print('Usted es menor de edad')
+
+# HUA que indique si un estudiante aprobo o reprobo una
+# asignatura teniendo en cuenta que aprueba con minimo
+# una calificacion de 3.0 hasta 5.0
+
+nota_final = float(input('Digite la nota final: '))
+if(nota_final > 3.0 and nota_final < 5.0):
+    print('Usted aprobo la asignatura')
+elif(nota_final < 3.0 and nota_final > 0):
+    print('Usted reprobo la asignatura')
+else:
+    print('La nota ingresada no es valida')
+
+# HUA que dado un numero n diga si es negativo, positivo o cero.
+
+n = float(input('Digite un numero: '))
+if (n < 0):
+    print('El numero {n} es positivo')
+elif (n < 0):
+    print('El numero {n} es negativo')
+else:
+    print('El numero es cero')
+    
