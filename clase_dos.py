46d67cbb9a33067ea0072fb62f0635970cb09abd
Amanda Ocampo <aocampo@cuc.edu.co>
2021-09-06 19:38:26 -0500

    Tabla de verdad, jerarquia de operaciones y condiciones

--- clase_dos.py
+++ clase_dos.py
@@ -4,4 +4,21 @@ Created on Mon Sep  6 19:14:00 2021
 
 @author: amand
 """
+# Condicionales
 
+# Tabla de verdad
+
+# Tabla del and
+# v and v = v
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# Tabla del or
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
