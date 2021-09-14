1d4195daa1aa77a1cb7dc7b554315076cee8c264
Amanda Ocampo <aocampo@cuc.edu.co>
2021-09-13 20:03:25 -0500

    Tercera clase

--- clase_dos.py
+++ clase_dos.py
@@ -100,4 +100,21 @@ if(num >= 0):
     print(f'El promedio final es: {promedio}')
 else:
     print('El numero de notas no puede ser menor o igual a cero')
-    
+
+# HUA que calcule el total a pagar por la compra de camisas.
+# Si se compran tres camisas o mas se aplica un descuento del 30% sobre
+# el total de la compra y si son menos de tres camisas un descuento del 10%.
+
+cant = int(input('Digite la cantidad de camisas que desea comprar: '))
+precio = int(input('Digite el precio de las camisas: '))
+desc = 0
+sub_total = cant*precio
+if(cant >= 3):
+    desc = sub_total*0.30
+    print('Usted recibio un descuento del 30%')
+else:
+    desc = sub_total*0.10
+    print('Usted recibio un descuento del 10%')
+total = sub_total-desc
+print('El total a pagar es: ', total)
+print('El descuento realizado fue: ', desc)
